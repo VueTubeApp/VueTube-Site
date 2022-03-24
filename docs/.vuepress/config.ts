c811a8a70e2b784d995041ad3d88829173d72b8d
Kenny <27463495+Frontesque@users.noreply.github.com>
2022-03-24 14:09:18 -0400

    version stuff

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -15,7 +15,7 @@ export = defineConfig4CustomTheme<ThemeConfig>((ctx) => ({
         {
           title: "Install",
           collapsable: false,
-          children: ["/install/", "/install/nightly-install", "/install/unstable-install", "/install/android-support"],
+          children: ["/install/", "/install/nightly-install", "/install/unstable-install", "/install/android-support","/install/android-system-webview"],
         },
       ],
     },
---
+++ docs/install/android-system-webview.md
@@ -0,0 +1,6 @@
+# Android System WebView
+
+On some older devices, you may need to update your version of `Android System WebView` in order to get VueTube working properly. You can update your installed version via the link below.
+https://play.google.com/store/apps/details?id=com.google.android.webview
+
+You can also check if your device may need WebView updates [here](/install/android-support).
