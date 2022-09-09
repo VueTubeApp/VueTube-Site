be43aaa7b39f2139106d9d880c19791f76ddc5aa
Alex <56329333+404-Program-not-found@users.noreply.github.com>
2022-09-10 01:38:30 +1200

    Add path

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,53 +1,54 @@
-import { ThemeConfig } from "vuepress-theme-vt";
-import { defineConfig4CustomTheme } from "vuepress/config";
+import {ThemeConfig} from "vuepress-theme-vt";
+import {defineConfig4CustomTheme} from "vuepress/config";
 
 export = defineConfig4CustomTheme<ThemeConfig>((ctx) => ({
-  theme: "vt",
-  title: "VueTube",
-  head: [["link", { rel: "icon", href: "/logo.svg" }]],
-  themeConfig: {
-    enableDarkMode: true,
-    repo: "https://github.com/Frontesque/VueTube",
-    logo: "/logo.svg",
-    nav: [{ text: "Download", link: "/install/" }],
+    theme: "vt",
+    title: "VueTube",
+    head: [["link", {rel: "icon", href: "/logo.svg"}]],
+    themeConfig: {
+        enableDarkMode: true,
+        repo: "https://github.com/Frontesque/VueTube",
+        logo: "/logo.svg",
+        nav: [{text: "Download", link: "/install/"}],
 
 
-    sidebar: {
-      "/": [
-        {
-          title: "Install",
-          collapsable: false,
-          children: ["/install/", "/install/android-system-webview"],
+        sidebar: {
+            "/": [
+                {
+                    title: "Install",
+                    collapsable: false,
+                    children: ["/install/", "/install/android-system-webview"],
+                },
+                {
+                    title: "Supported Devices",
+                    collapsable: false,
+                    children: ["/support/android", "/support/ios"],
+                },
+                {
+                    title: "Development",
+                    collapsable: false,
+                    path: "/contributing/",
+                    children: ["/contributing/app.md", "/contributing/extractor.md", "/contributing/design.md", "/contributing/translation.md"],
+                },
+                {
+                    title: "Information",
+                    collapsable: false,
+                    children: ["/info/screenshots"],
+                },
+                {
+                    title: "Plugins",
+                    collapsable: false,
+                    children: ["/plugins/"],
+                }
+            ]
         },
-        {
-          title: "Supported Devices",
-          collapsable: false,
-          children: [ "/support/android", "/support/ios"],
-        },
-        {
-          title: "Development",
-          collapsable: false,
-          children: [ "/contributing/"],
-        },
-        {
-          title: "Information",
-          collapsable: false,
-          children: [ "/info/screenshots"],
-        },
-        {
-          title: "Plugins",
-          collapsable: false,
-          children: [ "/plugins/"],
-        }
-      ]
-    },
 
 
-    codeSwitcher: {
-      groups: {
-        default: { ts: "TypeScript", js: "JavaScript" },
-        "plugin-usage": { tuple: "Tuple", object: "Object" },
-      },
-    },
-  }
+        codeSwitcher: {
+            groups: {
+                default: {ts: "TypeScript", js: "JavaScript"},
+                "plugin-usage": {tuple: "Tuple", object: "Object"},
+            },
+        },
+    }
 }))
